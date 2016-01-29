85b091889963e316b3fa34fb673b744e842f1157
diarmaidm <diarmaidm@users.noreply.github.com>
2016-01-29 00:36:48 +0000

    Done some messing around with ssl test

--- test/ssl-test.js
+++ test/ssl-test.js
@@ -27,44 +27,51 @@ test.sslOptions = {
 };
 
 describe('SOAP Client(SSL)', function() {
-  before(function(done) {
-    fs.readFile(__dirname + '/wsdl/strict/stockquote.wsdl', 'utf8', function(err, data) {
-      assert.ok(!err);
-      test.wsdl = data;
-      done();
-    });
-  });
+  // before(function(done) {
+  //   fs.readFile(__dirname + '/wsdl/strict/stockquote.wsdl', 'utf8', function(err, data) {
+  //     assert.ok(!err);
+  //     test.wsdl = data;
+  //     done();
+  //   });
+  // });
 
-  beforeEach(function(done) {
-    test.server = https.createServer(test.sslOptions, function(req, res) {
-      res.statusCode = 404;
-      res.end();
-    }).listen(51515, function() {
-      test.soapServer = soap.listen(test.server, '/stockquote', test.service, test.wsdl);
-      test.baseUrl =
-        'https://' + test.server.address().address + ':' + test.server.address().port;
-      
-      if (test.server.address().address === '0.0.0.0' || test.server.address().address === '::') {
-        test.baseUrl =
-          'https://127.0.0.1:' + test.server.address().port;
-      }
-      done();
-    });
-  });
+  // beforeEach(function(done) {
+  //   test.server = https.createServer(test.sslOptions, function(req, res) {
+  //     res.statusCode = 404;
+  //     res.end();
+  //   }).listen(51515, function() {
+  //     test.soapServer = soap.listen(test.server, '/stockquote', test.service, test.wsdl);
+  //     test.baseUrl =
+  //       'https://' + test.server.address().address + ':' + test.server.address().port;
 
-  afterEach(function(done) {
-    test.server.close(function() {
-      test.server = null;
-      delete test.soapServer;
-      test.soapServer = null;
-      done();
-    });
-  });
+  //     if (test.server.address().address === '0.0.0.0' || test.server.address().address === '::') {
+  //       test.baseUrl =
+  //         'https://127.0.0.1:' + test.server.address().port;
+  //     }
+  //     done();
+  //   });
+  // });
+
+  // afterEach(function(done) {
+  //   test.server.close(function() {
+  //     test.server = null;
+  //     delete test.soapServer;
+  //     test.soapServer = null;
+  //     done();
+  //   });
+  // });
 
   it('should connect to an SSL server', function(done) {
-    soap.createClient(__dirname + '/wsdl/strict/stockquote.wsdl', function(err, client) {
+    // soap.createClient(__dirname + '/wsdl/strict/stockquote.wsdl', function(err, client) {
+    soap.createClient('https://apitest.authorize.net/soap/v1/Service.asmx?WSDL', function(err, client) {
+      console.log('after created the client:', client);
+      console.log('*1*********************************************************************************');
       assert.ok(!err);
-      client.setEndpoint(test.baseUrl + '/stockquote');
+      console.log('*2*********************************************************************************');
+// "https://api.authorize.net/soap/v1/IsAlive"
+      // client.setEndpoint(test.baseUrl + '/stockquote');
+      client.setEndpoint("https://api.authorize.net/soap/v1/IsAlive");
+      console.log('*3*********************************************************************************');
       client.setSecurity({
         addOptions:function(options){
           options.cert = test.sslOptions.cert,
@@ -76,10 +83,12 @@ describe('SOAP Client(SSL)', function() {
         },
         toXML: function() { return ''; }
       });
+      console.log('*4*********************************************************************************');
 
-      client.GetLastTradePrice({ tickerSymbol: 'AAPL'}, function(err, result) {
+      client.IsAlive('', function(err, result) {
+        console.log('*5*********************************************************************************');
         assert.ok(!err);
-        assert.equal(19.56, parseFloat(result.price));
+        // assert.equal(19.56, parseFloat(result.price));
         done();
       });
     });
